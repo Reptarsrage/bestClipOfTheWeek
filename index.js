c2eb5ea25ad05aa3457fb81e61d02587442a42c3
Justin Robb <justinr@maqsoftware.com>
2015-04-02 17:54:14 +0000

    Added recommended videos list. Hooked this list up to the input field. Updated sorting/formatting logic on the results list. Updated the error show/hide logic for consistency.
    git-tfs-id: [https://maqbootcampjuly14.visualstudio.com/DefaultCollection]$/Test Project/WebSites/YouTubeAPISite;C74

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const OKAY = 2
 const TIMER_DELAY = 2500;
 const MAX_TIMER_COUNT = 120 / (TIMER_DELAY / 1000);
 const BAR_CHART_MAX_RATIO = 0.5;
+const PLAYLIST_TITLE = "World's Best Clip of the Week"
 
 const TERMS = Array('Alpha'
 	, 'Bravo'
@@ -34,7 +35,7 @@ const TERMS = Array('Alpha'
 
 var startTime, endTime;
 var loaded = false;
-var colorArray;
+var colorArray, termStats;
 var data = null;
 var commentHTML = $("<div></div>");
 var overallCount = 0;
@@ -59,12 +60,125 @@ google.setOnLoadCallback(function () {
 function onJSClientLoad() {
     gapi.client.setApiKey('AIzaSyB_LOatFV88Yptvdv_ot_yvoQ9MZDKgdzE');
     gapi.client.load('plus', 'v1');
-    gapi.client.load('youtube', 'v3');
+    gapi.client.load('youtube', 'v3').then(function () {
+        populateBestOfTheWeek();
+    });
 }
 
 function executeAsync(func) {
     setTimeout(func, 0);
 }
+
+function populateBestOfTheWeek() {
+    var select = $("#select_bestOfTheWeek");
+
+    var request = gapi.client.youtube.channels.list({
+        forUsername: 'StoneMountain64',
+        part: 'id'
+    });
+    request.execute(function (response) {
+        channelID = response.result.items[0].id;//relatedPlaylists.uploads;
+        requestVideoPlaylist(channelID);
+
+    }, function (reason) {
+        select.append("<li>Error retrieving videos</li>");
+        select.attr("disabled", 'disabled');
+    });
+}
+
+
+// Retrieve the list of videos in the specified playlist.
+function requestVideoPlaylist(channelID, pageToken) {
+    var requestOptions = {
+        channelId: channelID,
+        part: 'snippet',
+        maxResults: 20
+    };
+    if (pageToken) {
+        requestOptions.pageToken = pageToken;
+    }
+    var request = gapi.client.youtube.playlists.list(requestOptions);
+    request.execute(function (response) {
+        // Only show pagination buttons if there is a pagination token for the
+        // next or previous page of results.
+        nextPageToken = response.result.nextPageToken;
+        var playlistItems = response.result.items;
+        if (playlistItems) {
+            $.each(playlistItems, function (index, item) {
+                var title = item.snippet.title;
+                var id = item.id;
+                if (title == PLAYLIST_TITLE) {
+                    requestVideosInPlaylist(id);
+                    return;
+                }
+            });
+        }
+        if (nextPageToken)
+            requestVideoPlaylist(channelID, nextPageToken);
+
+    }, function (reason) {
+        select.append("<option value='Error'>Error retrieving videos</option>");
+        select.attr("disabled", 'disabled');
+    });
+}
+
+
+// Retrieve the list of videos in the specified playlist.
+function requestVideosInPlaylist(playlistId, pageToken) {
+    var requestOptions = {
+        playlistId: playlistId,
+        part: 'snippet',
+        maxResults: 20
+    };
+    if (pageToken) {
+        requestOptions.pageToken = pageToken;
+    }
+    var request = gapi.client.youtube.playlistItems.list(requestOptions);
+    request.execute(function (response) {
+        // Only show pagination buttons if there is a pagination token for the
+        // next or previous page of results.
+        nextPageToken = response.result.nextPageToken;
+
+        var playlistItems = response.result.items;
+        if (playlistItems) {
+            $.each(playlistItems, function (index, item) {
+                date = new Date(item.snippet.publishedAt);
+                title = item.snippet.title;
+                pos = item.snippet.position;
+                url = item.snippet.thumbnails.default.url;
+                id = item.snippet.resourceId.videoId;
+
+                if (title.length > 80) {
+                    title = title.substring(0, 87) + "...";
+                }
+
+
+                content = $("<li class='option' onclick='addUrlToInput(\"https://www.youtube.com/watch?v=" + id + "\", this)' title='" + item.snippet.title + "'></li>");
+                content.append($("<img class='option_thumb' src='" + url + "' alt='" + pos + "' \>"));
+                content.append($("<h3 class='option_title' >" + title + "<br><p class='option_date'>Date added: " + date.toLocaleDateString() + "</p></h3>"));
+
+                $("#select_bestOfTheWeek").append(content);
+            });
+        }
+
+        if (nextPageToken)
+            requestVideosInPlaylist(playlistId, nextPageToken);
+    });
+}
+
+function addUrlToInput(url, elt) {
+    if ($(elt).hasClass("selected")) {
+        $(elt).removeClass("selected");
+        $("#input_video").val("");
+        return true;
+    }
+
+    $("#select_bestOfTheWeek .selected").removeClass("selected");
+    $(elt).addClass("selected");
+
+    $("#input_video").val(url);
+}
+
 
 function drawChart(chartType, containerID, dataTablo, options) {
     var containerDiv = document.getElementById(containerID);
@@ -119,8 +233,7 @@ function toggleComments(cb, manual) {
         toggleVoters($("#checkbox_voters"), false);
     }
 
-    if (cb.checked) {
-       // $("#comments").empty();
+    if (cb.attr('checked') == 'checked') {
         $("#comments").append(commentHTML);
         $("#comments").show();
     } else {
@@ -136,7 +249,7 @@ function toggleVoters(cb, manual) {
         toggleComments($("#checkbox_comments"), false);
     }
 
-    if (cb.checked) {
+    if (cb.attr('checked') == 'checked') {
         $("#voters").show();
     } else {
         $("#voters").hide();
@@ -161,29 +274,44 @@ function hideMessage() {
 }
 
 function showErrors() {
-    $("#stats").append("<h3 class='error'>" + "Unable to find stats for your video. Make sure the url you have provided is valid and contains a valid video ID. (For example: https://www.youtube.com/watch?<b>v=1M5vGlvic_o</b>)" + "</h3>");
-    $("#termSpace").append("<h3 class='error'>" + "There are no results to display for your video." + "</h3>");
-    $("#commentSpace").append("<h3 class='error'>" + "Unable to find comments for your video." + "</h3>");
+    $("#chartSection").hide();
+    $("#termResults").hide();
+    $("#termSpace > .error").show();
+    $("#commentSpace > .error").show();
+    $("#stats > .error").show();
+
+   // $("#stats").append("<h3 class='error'>" + "Unable to find stats for your video. Make sure the url you have provided is valid and contains a valid video ID. (For example: https://www.youtube.com/watch?<b>v=1M5vGlvic_o</b>)" + "</h3>");
+    //$("#termSpace").append("<h3 class='error'>" + "There are no results to display for your video." + "</h3>");
+    //$("#commentSpace").append("<h3 class='error'>" + "Unable to find comments for your video." + "</h3>");
     $("#termSpace").removeClass("hidden");
     $("#commentSpace").removeClass("hidden");
     $("#results").removeClass("hidden");
+    $("input[type='checkbox']").attr("disabled", true);
                 
 }
 
 function fetchResults() {
     // clean up
+    $("input[type='checkbox']").removeAttr("disabled");
     $("#commentSpace").addClass("hidden");
     $("#termSpace").addClass("hidden");
     $("#results").addClass("hidden");
-    $(".error").remove();
-    $("#termResults").empty();
-    $("#stats").empty();
-    $("#voters").empty();
+    //$(".error").remove();
+    $("#termResults_list").empty();
+    $("#stats").children().filter(":not(.error)").remove();
+    $("#voters .error").show();
     $("#comments").empty();
+    $("#chartSection").show();
+    $("#termResults").show();
+    $("#h2_comments").html('0');
+    $("#chartSection").hide();
+    $("#termResults").hide();
+    $("#termSpace").removeClass("hidden");
+    $("#voters").children().filter(":not(.error)").remove();
 
     //reset
     loaded = false;
-    colorArray = null;
+    colorArray, termStats = null;
     commentHTML = $("<div></div>");
     overallCount = 0;
     lastCount = 0;
@@ -192,6 +320,8 @@ function fetchResults() {
     maxValue = 500;
     data = new google.visualization.DataTable();
     voters = new Array();
+    toggleVoters($("#checkbox_voters"), false);
+    toggleComments($("#checkbox_comments"), false);
 
     // Start
     fetchID++;
@@ -199,7 +329,6 @@ function fetchResults() {
 
     displayMessage('Processing query...please wait', OKAY);
     var id = grabVideoId();
-    $("#termSpace").append("<h3 class='error'>" + "There are no results to display for your video." + "</h3>");
     executeAsync(getVideoStats(id, fetchID));
     chartTimeUpdate(fetchID);
 }
@@ -277,10 +406,14 @@ function getVideoStats(id, currFetchID) {
             videoStats.append(viewCount).append(dislikeCount).append(favoriteCount).append(commentCount);
 
             // add to DOM
+            $("#stats > .error").hide();
             $("#stats").append(title).append(description).append(image)
             $("#stats").append(videoStats);
             $("#results").removeClass("hidden");
             executeAsync(loadComments(1, "http://gdata.youtube.com/feeds/api/videos/" + id + "/comments?v=2&alt=json&max-results=" + 20, currFetchID));
+
+
+
         } else {
             // no results
             showErrors();
@@ -323,8 +456,8 @@ function loadComments(count, url, currFetchID) {
             console.log(errorThrown);
             console.log(jqXHR);
             displayMessage("Woops! Error retrieving comments. (" + errorThrown + ")", BAD);
-            $("#termSpace").append("<h3 class='error'>" + "There are no results to display for your video." + "</h3>").removeClass("hidden");
-            $("#commentSpace").append("<h3 class='error'>" + "Unable to find comments for your video." + "</h3>").removeClass("hidden");
+            $("#commentSpace > .error").show();
+            $("input[type='checkbox']").attr("disabled", true);
         },
         success: function (data) {
             if (currFetchID != fetchID)
@@ -333,6 +466,7 @@ function loadComments(count, url, currFetchID) {
             nextUrl = getNextPageUrl(data);
             console.log(nextUrl);
             $.each(data.feed.entry, function (key, val) {
+                $("#commentSpace > .error").hide();
                 $("#commentSpace").removeClass("hidden");
                 var comment = $("<li class='comment'></li>");
                 var body = $("<div class='commentBody'></div>");
@@ -345,7 +479,9 @@ function loadComments(count, url, currFetchID) {
                 // reply count: val.yt$replyCount.$t
 
 
-                var googleID = val.yt$googlePlusUserId.$t;
+                var googleID;
+                if (val.hasOwnProperty('yt$googlePlusUserId'))
+                    googleID = val.yt$googlePlusUserId.$t;
 
                 var replyCt = 0;
                 var commentID = "N/A";
@@ -373,6 +509,7 @@ function loadComments(count, url, currFetchID) {
 
                 body.append(author).append(content).append(userData);
                 comment.append(body);
+                $("#comments .error").remove();
                 commentHTML.append(comment);
                 $("#h2_comments").html(count);
                 count++;
@@ -450,6 +587,7 @@ function parseComment(comment, currFetchID, author) {
 
     if (!loaded) {
         colorArray = Array(TERMS.length);
+        termStats = Array(TERMS.length);
 
         data.addColumn('string', 'Video');
         data.addColumn('number', 'Votes');
@@ -457,6 +595,9 @@ function parseComment(comment, currFetchID, author) {
         // Add rows
         data.addRows(TERMS.length);
         for (i = 0; i < TERMS.length; i++) {
+            // terms
+            termStats[i] = 0
+
 
             // data
             data.setCell(i, 0, TERMS[i]);
@@ -485,6 +626,7 @@ function parseComment(comment, currFetchID, author) {
     for (i = 0; i < TERMS.length; i++) {
         if ((index = comment.indexOf(TERMS[i].toLowerCase())) > -1) {
             data.setCell(i, 1, data.getValue(i, 1) + 1);
+            termStats[i]++;
 
             s = res.substring(0, index);
             t = res.substring(index, index + TERMS[i].length);
@@ -492,32 +634,44 @@ function parseComment(comment, currFetchID, author) {
 
             res = s + "<span class='" + TERMS[i] + " highlight'>" + t + "</span>" + v;    
             comment = res.toLowerCase();
-            $("#termResults ." + TERMS[i]).remove();
-            $("#termResults").append("<li class='" + TERMS[i] + "'>" + TERMS[i] + ": " + data.getValue(i, 1) + "</li>");
-            $("#termSpace .error").remove();
+            $("#termResults_list ." + TERMS[i]).remove();
+            $("#termResults_list").append("<li class='" + TERMS[i] + "'>" + TERMS[i] + ": " + data.getValue(i, 1) + "</li>");
+            $("#termSpace > .error").hide();
+            $("#chartSection").show();
+            $("#termResults").show();
+
 
             if (voters.indexOf(author) < 0) {
                 voters.push(author);
+                $("#voters .error").hide();
                 addToSortedList("voters", author);
             }
         }
     }
 
+    $("#termResults .remove").remove();
     // sort
-    var mylist = $('#termResults');
+    var mylist = $('#termResults_list');
     var listitems = mylist.children('li').get();
+    var numberPattern = /\d+/g;
     listitems.sort(function (a, b) {
-        return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+        anum = parseInt($(a).text().match(numberPattern)[0]);
+        bnum = parseInt($(b).text().match(numberPattern)[0]);
+
+        if (anum != bnum)
+            return bnum - anum;
+        else
+            return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
     })
     $.each(listitems, function (idx, itm) { mylist.append(itm); });
 
     //// add the rest
-    //for (i = 0; i < TERMS.length; i++) {
-    //    if (termStats[i] == 0) {
-    //        $("#termResults ." + TERMS[i]).remove();
-    //        $("#termResults").append("<li class='" + TERMS[i] + "'>" + TERMS[i] + ": " + termStats[i] + "</li>");
-    //    }
-    //}
+    $("#termResults_list").append("<li class='remove' style='color: gray'>&nbsp</li>");
+    for (i = 0; i < TERMS.length; i++) {
+        if (termStats[i] == 0) {
+            $("#termResults_list").append("<li class='remove' style='color: gray'>" + TERMS[i] + ": 0</li>");
+        }
+    }
 
     $("#termSpace").removeClass("hidden");
     overallCount++;
