426293c5e212081f87e4b1cc028cdd79afba7cc1
Justin Robb <reptarsrage@gmail.com>
2018-09-30 15:06:37 -0700

    Added react switch and improved logic

--- src/BestClipOfTheWeek/js/Terms/components/Index.jsx
+++ src/BestClipOfTheWeek/js/Terms/components/Index.jsx
@@ -1,6 +1,8 @@
+/* global $ */
 import React, { Component } from 'react';
 import { GridLoader } from 'halogenium';
 import { SketchPicker } from 'react-color';
+import Switch from 'react-switch';
 
 import TermsService from '../../termsService';
 
@@ -33,8 +35,10 @@ export default class Index extends Component {
     this.handleColorSwatchClick = this.handleColorSwatchClick.bind(this);
     this.handleColorPickerClose = this.handleColorPickerClose.bind(this);
     this.handleColorPickerChangeComplete = this.handleColorPickerChangeComplete.bind(this);
-    this.handleTermChange = this.handleTermChange.bind(this);
-    this.handleNewTermChange = this.handleNewTermChange.bind(this);
+    this.handleTermNameChange = this.handleTermNameChange.bind(this);
+    this.handleTermEnabled = this.handleTermEnabled.bind(this);
+    this.handleNewTermEnabled = this.handleNewTermEnabled.bind(this);
+    this.handleNewTermNameChange = this.handleNewTermNameChange.bind(this);
     this.handleNewTermCreate = this.handleNewTermCreate.bind(this);
     this.handleTermDelete = this.handleTermDelete.bind(this);
     this.getTermFromTarget = this.getTermFromTarget.bind(this);
@@ -50,10 +54,11 @@ export default class Index extends Component {
   }
 
   async componentDidMount() {
+    // Fetch all terms
     const { termsService } = this.state;
-
     const termResults = await termsService.getTerms();
 
+    // Save them in state
     this.setState(prevState => ({
       ...prevState,
       termsState: {
@@ -66,11 +71,16 @@ export default class Index extends Component {
   }
 
   getTermFromTarget(e) {
+    // Get id from closest row to target
+    const { target } = e;
+    const id = $(target)
+      .closest('.term-form-row')
+      .data('id');
+    const termId = parseInt(id, 10);
+
+    // Find term in list
     const { termsState } = this.state;
     const { terms } = termsState;
-    const { dataset } = e.target;
-    const { id } = dataset;
-    const termId = parseInt(id, 10);
     return terms.find(t => t.termId === termId);
   }
 
@@ -132,7 +142,29 @@ export default class Index extends Component {
     }));
   }
 
-  async handleTermChange(e) {
+  async handleTermEnabled(checked, event) {
+    if (!event || !event.target) {
+      return;
+    }
+
+    // Parse target term
+    const term = this.getTermFromTarget(event);
+    if (!term) {
+      return;
+    }
+
+    // Update the term and force state update
+    const { termsService } = this.state;
+    term.enabled = checked;
+
+    // Update UI
+    this.forceUpdate();
+
+    // Upddate server
+    await termsService.updateTerm(term);
+  }
+
+  async handleTermNameChange(e) {
     if (!e || !e.target) {
       return;
     }
@@ -145,23 +177,13 @@ export default class Index extends Component {
 
     // Update the term and force state update
     const { termsService } = this.state;
-    if (e.target.name === 'name') {
-      term.name = e.target.value;
+    term.name = e.target.value;
 
-      // Update UI
-      this.forceUpdate();
+    // Update UI
+    this.forceUpdate();
 
-      // Upddate server
-      await termsService.updateTerm(term);
-    } else if (e.target.name === 'enabled') {
-      term.enabled = e.target.checked;
-
-      // Update UI
-      this.forceUpdate();
-
-      // Upddate server
-      await termsService.updateTerm(term);
-    }
+    // Upddate server
+    await termsService.updateTerm(term);
   }
 
   handleColorSwatchClick(e) {
@@ -194,13 +216,13 @@ export default class Index extends Component {
     }));
   }
 
-  async handleTermDelete(e) {
-    if (!e || !e.target) {
+  async handleTermDelete(event) {
+    if (!event || !event.target) {
       return;
     }
 
     // Parse the target term
-    const term = this.getTermFromTarget(e);
+    const term = this.getTermFromTarget(event);
     if (!term) {
       return;
     }
@@ -210,7 +232,7 @@ export default class Index extends Component {
     let { terms } = termsState;
     terms = terms.filter(({ termId }) => termId !== term.termId);
 
-    // Update UI
+    // Update state
     this.setState(prevState => ({
       ...prevState,
       termsState: {
@@ -219,28 +241,31 @@ export default class Index extends Component {
       },
     }));
 
-    // Upddate server
+    // Update server
     await termsService.deleteTerm(term.termId);
   }
 
-  handleNewTermChange(e) {
-    if (!e || !e.target) {
-      return;
-    }
-
-    const { newTerm } = this.state;
-
-    if (e.target.name === 'name') {
-      newTerm.name = e.target.value;
-    } else if (e.target.name === 'enabled') {
-      newTerm.enabled = e.target.checked;
-    }
-
+  handleNewTermEnabled(checked) {
     this.setState(prevState => ({
       ...prevState,
       newTerm: {
         ...prevState.newTerm,
-        ...newTerm,
+        enabled: checked,
+      },
+    }));
+  }
+
+  handleNewTermNameChange(event) {
+    if (!event || !event.target) {
+      return;
+    }
+
+    const { target } = event;
+    this.setState(prevState => ({
+      ...prevState,
+      newTerm: {
+        ...prevState.newTerm,
+        name: target.value,
       },
     }));
   }
@@ -291,7 +316,7 @@ export default class Index extends Component {
       colorPickerElt = (
         <div className="popover">
           <div role="button" className="cover" onClick={this.handleColorPickerClose} onKeyDown={this.handleKeyDown} tabIndex="0" />
-          <SketchPicker className="relative" color={colorPickerColor} onChangeComplete={this.handleColorPickerChangeComplete} />
+          <SketchPicker className="relative" color={colorPickerColor} onChangeComplete={this.handleColorPickerChangeComplete} disableAlpha />
         </div>
       );
     }
@@ -299,21 +324,21 @@ export default class Index extends Component {
     // Build table rows from terms
     const selectedTermId = selectedTerm.termId;
     const termEltsList = terms.map(term => (
-      <div key={term.termId} className="form-row term-form-row ease">
+      <div key={term.termId} className="form-row term-form-row ease" data-id={term.termId}>
         <div className="form-group d-flex">
-          <input className="form-control" data-id={term.termId} maxLength="50" name="name" onChange={this.handleTermChange} placeholder={term.name} type="text" value={term.name} />
+          <input className="form-control" maxLength="50" onChange={this.handleTermNameChange} placeholder={term.name} type="text" value={term.name} />
         </div>
         <div className="form-group d-flex">
-          <div role="button" className="swatch" data-id={term.termId} onClick={this.handleColorSwatchClick} onKeyDown={this.handleKeyDown} tabIndex="0">
-            <div className="swatch-color" data-id={term.termId} style={{ background: term.color }} />
+          <div role="button" className="swatch" onClick={this.handleColorSwatchClick} onKeyDown={this.handleKeyDown} tabIndex="0">
+            <div className="swatch-color" style={{ background: term.color }} />
             {selectedTermId === term.termId ? colorPickerElt : null}
           </div>
         </div>
         <div className="form-group d-flex">
-          <input checked={term.enabled} data-id={term.termId} name="enabled" onChange={this.handleTermChange} type="checkbox" />
+          <Switch onChange={this.handleTermEnabled} checked={term.enabled} />
         </div>
         <div className="form-group d-flex">
-          <button type="button" className="btn btn-danger" data-id={term.termId} onClick={this.handleTermDelete}>
+          <button type="button" className="btn btn-danger term-form-button" onClick={this.handleTermDelete}>
             Delete
           </button>
         </div>
@@ -322,21 +347,27 @@ export default class Index extends Component {
 
     return (
       <div className="container-fluid">
-        <div className="form-row term-form-row ease">
+        <div className="form-row term-form-row ease bg-primary text-white py-2 mb-2">
+          <strong className="d-flex">Name</strong>
+          <strong className="d-flex">Color</strong>
+          <strong className="d-flex">Enabled</strong>
+          <strong className="d-flex">&nbsp;</strong>
+        </div>
+        <div className="form-row term-form-row ease" data-id={newTerm.termId}>
           <div className="form-group d-flex">
-            <input className="form-control" maxLength="50" name="name" onChange={this.handleNewTermChange} placeholder="Add a term" type="text" value={newTerm.name} />
+            <input className="form-control" maxLength="50" onChange={this.handleNewTermNameChange} placeholder="Add a term" type="text" value={newTerm.name} />
           </div>
           <div className="form-group d-flex">
-            <div role="button" className="swatch" data-id={newTerm.termId} onClick={this.handleColorSwatchClick} onKeyDown={this.handleKeyDown} tabIndex="0">
-              <div className="swatch-color" data-id={newTerm.termId} style={{ background: newTerm.color }} />
+            <div role="button" className="swatch" onClick={this.handleColorSwatchClick} onKeyDown={this.handleKeyDown} tabIndex="0">
+              <div className="swatch-color" style={{ background: newTerm.color }} />
               {selectedTermId === newTerm.termId ? colorPickerElt : null}
             </div>
           </div>
           <div className="form-group d-flex">
-            <input checked={newTerm.enabled} name="enabled" onChange={this.handleNewTermChange} type="checkbox" />
+            <Switch onChange={this.handleNewTermEnabled} checked={newTerm.enabled} />
           </div>
           <div className="form-group d-flex">
-            <button type="button" className="btn btn-success" onClick={this.handleNewTermCreate}>
+            <button type="button" className="btn btn-success term-form-button" onClick={this.handleNewTermCreate}>
               Add
             </button>
           </div>
