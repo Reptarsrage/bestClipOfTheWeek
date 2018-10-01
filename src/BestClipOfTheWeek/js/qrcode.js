6f63bbf5157479d600ac3cbe03f7ab3dc0661617
Justin Robb <reptarsrage@gmail.com>
2018-09-30 18:06:11 -0700

    Fixing qr code url

--- src/BestClipOfTheWeek/js/qrcode.js
+++ src/BestClipOfTheWeek/js/qrcode.js
@@ -1,5 +1,7 @@
+/* global $ */
 import QRCode from 'qrcode';
 
 (() => {
-  QRCode.toCanvas(document.getElementById('qrCode'), '@Html.Raw(Model.AuthenticatorUri)');
+  const element = document.getElementById('qrCode');
+  QRCode.toCanvas(document.getElementById('qrCode'), $(element).data('url'));
 })();
