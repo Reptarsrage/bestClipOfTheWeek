1d34d30d786f9817c47d8c88f7056b64312cf11c
Justin Robb <reptarsrage@gmail.com>
2021-04-19 17:27:34 -0700

    Scaffolding out the registration to use default terms

---
+++ src/BestClipOfTheWeek/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BestClipOfTheWeek.Data;
+using BestClipOfTheWeek.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BestClipOfTheWeek.Areas.Identity.IdentityHostingStartup))]
+namespace BestClipOfTheWeek.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
